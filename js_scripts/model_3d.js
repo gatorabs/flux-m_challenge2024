37e38ba5a29ec382a95c909ac60fe355c59d276b
Gabriel Spioni <86626937+gatorabs@users.noreply.github.com>
2024-06-21 23:30:43 -0300

    v13.5 customizing 3d object

--- js_scripts/model_3d.js
+++ js_scripts/model_3d.js
@@ -1,5 +1,3 @@
-
-
 let scene, camera, renderer, object;
 let rotationX = 0;
 let rotationY = 0;
@@ -7,32 +5,42 @@ let rotationY = 0;
 function init() {
   const container = document.getElementById('3Dcube');
 
+  
   scene = new THREE.Scene();
 
   const aspectRatio = 430 / 445; // Proporção de 430x445 pixels
   camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
   camera.position.set(0, 0, 25);
 
+  
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(430, 445);
-  
-  // Define a cor de fundo do renderizador (no exemplo, um cinza claro)
   renderer.setClearColor(0x2C2C2C);
-
   container.appendChild(renderer.domElement);
 
-  const ambientLight = new THREE.AmbientLight(0xffffff); // luz branca total
+  
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); 
   scene.add(ambientLight);
 
   const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
   directionalLight.position.set(1, 1, 1).normalize();
   scene.add(directionalLight);
 
+  // Carregar modelo STL
   const loader = new THREE.STLLoader();
   loader.load('./AU20_Scania2.stl', function (geometry) {
     console.log('STL carregado com sucesso');
     geometry.computeVertexNormals();
-    const material = new THREE.MeshPhongMaterial({ color: 0x00ffff });
+    
+    
+    const material = new THREE.MeshPhongMaterial({
+      color: 0x00ffff,
+      shininess: 100,
+      specular: 0x111111,
+      side: THREE.DoubleSide 
+    });
+
+    
     object = new THREE.Mesh(geometry, material);
     object.position.set(0, 0, 0);
     object.scale.set(0.1, 0.1, 0.1);
@@ -42,8 +50,10 @@ function init() {
     console.error('Erro ao carregar o arquivo STL:', error);
   });
 
+  
   window.addEventListener('resize', onWindowResize, false);
 }
+
 
 function onWindowResize() {
   const container = document.getElementById('3Dcube');
@@ -53,25 +63,30 @@ function onWindowResize() {
   renderer.setSize(430, 445);
 }
 
+
 function animate() {
   requestAnimationFrame(animate);
+  
+
   if (object) {
     object.rotation.x = rotationX;
     object.rotation.y = rotationY;
   }
+
+  
   renderer.render(scene, camera);
 }
 
+// Iniciar a cena e animação
 init();
 animate();
+
 
 export function setRotationX(value) {
   rotationX = value;
 }
 
-function setRotationY(value) {
+
+export function setRotationY(value) {
   rotationY = value;
 }
-
-
-
