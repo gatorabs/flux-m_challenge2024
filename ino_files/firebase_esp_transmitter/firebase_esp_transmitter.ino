7418ac97bce33020b7b3d7a5bc6460ee40e816b9
Gabriel Spioni <gabrielprivate123@gmail.com>
2024-06-28 07:30:41 -0300

    updating esp32 v2.

--- ino_files/firebase_esp_transmitter/firebase_esp_transmitter.ino
+++ ino_files/firebase_esp_transmitter/firebase_esp_transmitter.ino
@@ -6,7 +6,8 @@
 #include <Arduino.h>
 #include <WiFi.h>               
 #include <Firebase_ESP_Client.h>
-#include <DHTesp.h>               
+#include <DHTesp.h> 
+#include<Wire.h>              
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
@@ -31,6 +32,9 @@ unsigned long sendDataPrevMillis = 0;
 int count = 0;
 bool signupOK = false;                      
 bool is_laserOn = true;
+
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
 void setup(){
   
   dht.setup(DHTPIN, DHTesp::DHT11);
@@ -38,6 +42,13 @@ void setup(){
   pinMode(FIRE_SENSOR, INPUT);
   pinMode(MQ3_PIN, OUTPUT);
   Serial.begin(115200);
+  
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);  
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED){
@@ -76,38 +87,52 @@ unsigned long previousMillis = 0;
 void loop(){
  unsigned long currentMillis = millis(); 
 
-if (currentMillis - previousMillis >= 100) {
-  previousMillis = currentMillis;
-  float humidity = dht.getHumidity();
-  float temperature = dht.getTemperature();
-  int ldr_reading = analogRead(LDR_PIN);
-  int ldr_mapping = map(ldr_reading, 0, 3000, 0, 100);
-  int mq4_reading = analogRead(MQ4_PIN);
-  int mq4_mapping = map(mq4_reading, 0, 2500, 0, 100);
-  int fire_sensor_reading = digitalRead(FIRE_SENSOR);
-  
-  
-  
-  //bool alchool_sensor_reading = digitalRead(MQ3_PIN);
-  
-  Serial.println(mq4_reading);
-  
-  
-
-  
-  //dht_sensor_fb(temperature, humidity);
-  firebase_sending_function("MQ4", mq4_mapping);
-  firebase_sending_function("LDR", ldr_mapping);
-  firebase_sending_function("DHT11/Temperature", temperature);
-  firebase_sending_function("DHT11/Humidity", humidity);
-  firebase_sending_function("FIRE", fire_sensor_reading);
-
-  
-  
-  }
-
+ if (currentMillis - previousMillis >= 100) {
+   previousMillis = currentMillis;
+   float humidity = dht.getHumidity();
+   float temperature = dht.getTemperature();
+    
+   int ldr_reading = analogRead(LDR_PIN);
+   int ldr_mapping = map(ldr_reading, 0, 3000, 0, 100);
+   int mq3_reading = digitalRead(MQ3_PIN);
+    
+   int mq4_reading = analogRead(MQ4_PIN);
+   int mq4_mapping = map(mq4_reading, 0, 2500, 0, 100);
+    
+   int fire_sensor_reading = digitalRead(FIRE_SENSOR);
+   gyroscope_reading();
+   
+    
+   firebase_sending_function("MQ4", mq4_mapping);
+   firebase_sending_function("LDR", ldr_mapping);
+   firebase_sending_function("DHT11/Temperature", temperature);
+   firebase_sending_function("DHT11/Humidity", humidity);
+   firebase_sending_function("FIRE", fire_sensor_reading);
+   
+   firebase_sending_function("GYRO/x", GyX);
+   firebase_sending_function("GYRO/y", GyY);
+   firebase_sending_function("GYRO/z", GyZ);
+    
+   firebase_sending_function("MQ3", MQ3_PIN);
+ }
 }
 
+void gyroscope_reading(){
+  
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,12,true); 
+  
+  AcX=Wire.read()<<8|Wire.read();    
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read();
+    
+  GyX=Wire.read()<<8|Wire.read();  
+  GyY=Wire.read()<<8|Wire.read();  
+  GyZ=Wire.read()<<8|Wire.read();  
+  
+}
 
 void firebase_sending_function(String firebase_ref, int sensor_ref){
   if (Firebase.RTDB.setInt(&fbdo, firebase_ref, sensor_ref)){
