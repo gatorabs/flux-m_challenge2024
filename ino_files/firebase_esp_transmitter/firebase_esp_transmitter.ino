a45cb60ac5814cc9663c246fc355dd22f4d3d970
Gabriel Spioni <gabrielprivate123@gmail.com>
2024-09-19 11:05:23 -0300

    v19

--- ino_files/firebase_esp_transmitter/firebase_esp_transmitter.ino
+++ ino_files/firebase_esp_transmitter/firebase_esp_transmitter.ino
@@ -52,36 +52,7 @@ void setup(){
   Wire.write(0);    
   Wire.endTransmission(true);
   
-  Serial.println("Selecione uma rede Wi-Fi:");
-  for (int i = 0; i < 3; i++) {
-    Serial.print(i + 1);
-    Serial.print(": ");
-    Serial.println(wifi_ssid[i]);
-  }
-
-  int escolha = 0;
-  while (escolha < 1 || escolha > 3) {
-    while (Serial.available() == 0) {
-      
-    }
-    escolha = Serial.parseInt();
-  }
-  
-
-  Serial.print("Conectando-se à rede: ");
-  Serial.println(wifi_ssid[escolha - 1]);
-  
-  WiFi.begin(wifi_ssid[escolha - 1], wifi_password[escolha - 1]);
-
-  while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(300);
-  }
-
-  Serial.println("\nConectado!");
-  Serial.print("Endereço IP: ");
-  Serial.println(WiFi.localIP());
-
+  connectToWiFi();
  
   /* Assign the api key (required) */
   config.api_key = API_KEY;
@@ -103,14 +74,20 @@ void setup(){
   
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+  update_serial();
 }
 unsigned long previousMillis = 0;
+unsigned long intervalMillis = 60;
 
 void loop(){
  unsigned long currentMillis = millis(); 
+ 
 
- if (currentMillis - previousMillis >= 1) {
-   previousMillis = currentMillis;
+
+
+
+ if (currentMillis - previousMillis >= intervalMillis) {
+   Serial.println(intervalMillis);
    float humidity = dht.getHumidity();
    float temperature = dht.getTemperature();
     
@@ -140,6 +117,8 @@ void loop(){
    //firebase_sending_function("GYRO/z", GyZ);
     
    firebase_sending_function("MQ3", MQ3_PIN);
+
+   previousMillis = currentMillis;
  }
 }
 
@@ -170,3 +149,175 @@ void firebase_sending_function(String firebase_ref, int sensor_ref){
       Serial.println("REASON: " + fbdo.errorReason());
     }
 }
+
+
+
+void update_serial() {
+    bool flow_controller = false;
+    String inputString = "";  
+    unsigned long startTime = millis();  
+
+    Serial.println("Enter the new send TIME interval: ");
+    while (!flow_controller) {
+        if (Serial.available()) {
+            char incomingByte = Serial.read();
+            // receive a string via serial
+            //'\n' check if there was a line break
+            if (incomingByte == '\n') { 
+                int receivedValue = inputString.toInt();
+                Serial.print("Received Value: ");
+                Serial.println(receivedValue);
+                intervalMillis = receivedValue;
+                flow_controller = true;
+            } else {
+                inputString += incomingByte;
+            }
+
+            
+            startTime = millis();
+        }
+
+        
+        if (millis() - startTime >= 3000) {
+            Serial.println("No input received, using default value: 60");
+            intervalMillis = 60;  
+            flow_controller = true; 
+        }
+    }
+}
+
+
+void connectToWiFi() {
+    Serial.println("Escolha uma opção:");
+    Serial.println("1 - Scannear Wi-Fi da Proximidade");
+    Serial.println("2 - Conectar em Wi-Fi Salvos");
+
+    while (true) {
+        if (Serial.available()) {
+            int option = Serial.parseInt();
+            Serial.read(); 
+
+            if (option == 1) {
+                scanWiFiNetworks();
+                break;
+            } else if (option == 2) {
+                connectToSavedWiFi();
+                break;
+            } else {
+                Serial.println("Opção inválida! Tente novamente.");
+            }
+        }
+    }
+}
+
+void scanWiFiNetworks() {
+    Serial.println("Scaneando redes Wi-Fi...");
+    int numNetworks = WiFi.scanNetworks();
+    Serial.println("Redes encontradas:");
+
+    for (int i = 0; i < numNetworks; ++i) {
+        Serial.print(i + 1);
+        Serial.print(": ");
+        Serial.print(WiFi.SSID(i));
+        Serial.print(" (Sinal: ");
+        Serial.print(WiFi.RSSI(i));
+        Serial.println(" dBm)");
+    }
+
+    Serial.println("Escolha uma rede para conectar (1 a " + String(numNetworks) + "):");
+    while (true) {
+        if (Serial.available()) {
+            int networkChoice = Serial.parseInt();
+            Serial.read(); 
+
+            if (networkChoice >= 1 && networkChoice <= numNetworks) {
+                connectToNetwork(WiFi.SSID(networkChoice - 1));
+                break;
+            } else {
+                Serial.println("Escolha inválida! Tente novamente.");
+            }
+        }
+    }
+}
+
+void connectToNetwork(String ssid) {
+    Serial.print("Conectando à rede: ");
+    Serial.println(ssid);
+
+    int networkIndex = -1;
+    int numNetworks = WiFi.scanNetworks();
+
+    
+    for (int i = 0; i < numNetworks; ++i) {
+        if (WiFi.SSID(i) == ssid) {
+            networkIndex = i;
+            break;
+        }
+    }
+
+    // Verifica se a rede é protegida
+    if (networkIndex != -1 && WiFi.encryptionType(networkIndex) != WIFI_AUTH_OPEN) {
+        Serial.println("Esta rede requer senha. Digite a senha:");
+        String password = "";
+        while (true) {
+            if (Serial.available()) {
+                char c = Serial.read();
+                if (c == '\n') {
+                    break; 
+                } else {
+                    password += c; 
+                }
+            }
+        }
+
+        WiFi.begin(ssid.c_str(), password.c_str());
+    } else {
+        WiFi.begin(ssid.c_str());
+    }
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println();
+    Serial.println("Conectado com sucesso!");
+}
+
+void connectToSavedWiFi() {
+    Serial.println("Redes Wi-Fi salvas:");
+
+    for (int i = 0; i < sizeof(wifi_ssid) / sizeof(wifi_ssid[0]); i++) {
+        Serial.print(i + 1);
+        Serial.print(": ");
+        Serial.println(wifi_ssid[i]);
+    }
+
+    Serial.println("Escolha uma rede para conectar (1 a " + String(sizeof(wifi_ssid) / sizeof(wifi_ssid[0])) + "):");
+    while (true) {
+        if (Serial.available()) {
+            int savedNetworkChoice = Serial.parseInt();
+            Serial.read();
+
+            if (savedNetworkChoice >= 1 && savedNetworkChoice <= sizeof(wifi_ssid) / sizeof(wifi_ssid[0])) {
+                
+                String ssid = wifi_ssid[savedNetworkChoice - 1];
+                String password = wifi_password[savedNetworkChoice - 1];
+                WiFi.begin(ssid.c_str(), password.c_str());
+
+                while (WiFi.status() != WL_CONNECTED) {
+                    delay(500);
+                    Serial.print(".");
+                }
+
+                Serial.println();
+                Serial.println("Conectado com sucesso!");
+                break;
+            } else {
+                Serial.println("Escolha inválida! Tente novamente.");
+            }
+        }
+    }
+}
+
+
